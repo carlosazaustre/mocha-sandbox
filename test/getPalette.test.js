f27861b5395835bcf5d5686dd31a3e424ec9e387
carlosazaustre <cazaustre@gmail.com>
2015-01-11 14:00:24 +0100

    add more tests

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -3,6 +3,10 @@ var assert = require('assert');
 
 describe("getPalette", function() {
 
+  it("should throw an error if the result is not an array", function() {
+    assert.throws(getPalette, /is not an array/);
+  });
+
   it("should return an array with 3 items", function() {
     var palette = getPalette();
     assert(Array.isArray(palette), 'did not return an array');
